96be2da83be41441232f33c6b1f1f1896a9ed284
Fly J Gore <jennifer@theothergore.com>
2017-12-19 16:07:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-  const driver = {
-    name: 'Sam',
-    address: '11 Broadway',
-    city: 'New York',
-    state: 'NY',
-    zipCode: 10004
-  };
+function updateDriverWithKeyAndValue(){
+  const driver = { 
+    name: "Sam" }; 
+}
+
+
+
+
