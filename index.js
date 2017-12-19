add75acc2b4aee3795d22791609bd073c19b2816
Fly J Gore <jennifer@theothergore.com>
2017-12-19 16:13:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 const driver = {
   name: 'Sam'
 };
-
-function updateDriverWithKeyAndValue(driver, 'address', '11 Broadway'){
-  const driver = {
-    name: "Sam"
-    address: "11 Broadway"
-  };
-}
